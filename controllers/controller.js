28facd2d5716db4cdddd674708e266b83683cc48
Runner-Dad84 <vanhoogie@gmail.com>
2025-07-16 22:00:07 -0400

    chagen variable to item from comment

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,7 +2,7 @@ const db = require("../db/queries");
 
 async function returnAll(req, res) {
   try {
-  const comments = await db.returnAllItems();
+  const items = await db.returnAllItems();
   console.log("Items: ", items);
   res.render('index', { items });
   } catch (error) {
