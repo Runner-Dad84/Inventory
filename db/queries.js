0adc716572caf83edc0525df1f99d56c89758406
Runner-Dad84 <vanhoogie@gmail.com>
2025-07-20 21:27:45 -0400

    remove $1 in query. Change await to pool.query

--- db/queries.js
+++ db/queries.js
@@ -6,6 +6,14 @@ async function returnAllItems() {
   return rows;
 }
 
+//
+async function postItem (name, type, price, weather, description) {
+    query = 'INSERT INTO running_inventory (name, type, price, weather, description) VALUES ($1, $2, $3, $4, $5)';
+    await pool.query (query, [name, type, price, weather, description]);
+
+}
+
 module.exports = {
   returnAllItems,
+  postItem
 };
