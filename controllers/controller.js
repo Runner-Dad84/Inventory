2d3ed80382978255153bc1a4a1242d25e6f085b6
Runner-Dad84 <vanhoogie@gmail.com>
2025-07-20 21:26:57 -0400

    Fix function name to postItem. Fix parameters

--- controllers/controller.js
+++ controllers/controller.js
@@ -11,6 +11,22 @@ async function returnAll(req, res) {
   }
 }
 
+async function postInventory(req, res)
+ {
+  const { name, type, price, weather, description } = req.body;
+  console.log(req.body)
+  console.log('test 2');
+  try {
+    await db.postItem(name, type, price, weather, description);
+    console.log("Successfully added new item")
+    res.redirect('/');
+  } catch (err) {
+     console.error(err);
+    res.status(500).send("Error saving item.")
+  }
+
+ }
 module.exports = {
-    returnAll
+    returnAll,
+    postInventory
 }
