6f908634915f1c53400ecfe3bedbf4dc51a69ec5
Runner-Dad84 <vanhoogie@gmail.com>
2025-07-22 22:22:04 -0400

    return category array

--- db/queries.js
+++ db/queries.js
@@ -6,10 +6,16 @@ async function returnAllItems() {
   return rows;
 }
 
-//
+//add item
 async function postItem (name, type, price, weather, description) {
     query = 'INSERT INTO running_inventory (name, type, price, weather, description) VALUES ($1, $2, $3, $4, $5)';
     await pool.query (query, [name, type, price, weather, description]);
+}
+
+//Take any sized array of categories and return those categories
+async function returnMensShoe (categoryArray) {
+  const placeholders = categoryArray.map((__, i) => `$${i + 1}`.join(', '));
+  const { row } = await pool.query(`SELECT * FROM running_inventory WHERE type IN ${placeholders}`, categoryArray);
 
 }
 
