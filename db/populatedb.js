04e4d57674cc8e10b0e5c126fd796f339d4327a7
Runner-Dad84 <vanhoogie@gmail.com>
2025-07-22 20:49:49 -0400

    Add more rows

--- db/populatedb.js
+++ db/populatedb.js
@@ -12,8 +12,18 @@ CREATE TABLE IF NOT EXISTS running_inventory (
 
 INSERT INTO running_inventory (name, type, price, weather, description) 
 VALUES
-  ('Saucony Guide', 'Mens Shoe', '99.99', 'All', 'Moderate stability trainer'),
-  ('Racer Half Tights', 'Mens Apparel', '59.99', 'All', 'Great for a training days');
+  ('Saucony Guide', 'Mens Shoes', '124.99', 'All Weather', 'Moderate stability trainer'),
+  ('Saucony Guide', 'Womens Shoes', '124.99', 'All Weather', 'Moderate stability trainer'),
+  ('Ride Guide', 'Mens Shoes', '109.99', 'All Weather', 'Neutral, light weight, responsive'),
+  ('Ride Guide', 'Womens Shoes', '109.99', 'All Weather', 'Neutral, light weight, responsive'),
+  ('Triumph', 'Mens Shoes', '119.99', 'All Weather', 'Neutral, cushioned, plush'),
+  ('Triumph', 'Womens Shoes', '119.99', 'All Weather', 'Neutral, cushioned, plush'),
+  ('Racer Half Tights', 'Mens Apparel', '59.99', 'Summer', 'Great for racing days'),
+  ('Racer Half Tights', 'Womens Apparel', '59.99', 'Summer', 'Great for racing days'),
+  ('Full Tights', 'Mens Apparel', '84.99', 'Winter', 'Great for training or racing days'),
+  ('Full Tights', 'Womens Apparel', '84.99', 'Winter', 'Great for training or racing days'),
+  ('Race Singlet', 'Mens Apparel', '54.99', 'All Weather', 'Great for training or racing days'),
+  ('Race Singlet', 'Womens Apparel', '54.99', 'All Weather', 'Great for training or racing days');
 `;
 
 async function main() {
